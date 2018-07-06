872f81d8395e0c83a1145c7173168964b969f4c6
brapls <brianderg@gmail.com>
2018-07-06 16:28:16 +0200

    Submit

--- raceCarAnalyse/src/RCASMain.java
+++ raceCarAnalyse/src/RCASMain.java
@@ -1,26 +1,39 @@
 import java.util.ResourceBundle;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import rcas.model.RaceCar;
+import rcas.model.RaceCarSelectionItem;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 
 public class RCASMain extends Application {
 
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+
 		FXMLLoader fxmlLoader = new FXMLLoader();
 		fxmlLoader.setLocation(RCASMain.class.getResource("rcas/view/RCASMainView.fxml"));
 		ResourceBundle resourceBundle = ResourceBundle.getBundle("RCASResources");
 		fxmlLoader.setResources(resourceBundle);
-
 		GridPane mainPane = (GridPane) fxmlLoader.load();
 		Scene mainScene = new Scene(mainPane, 800, 600);
 		primaryStage.centerOnScreen();
 		primaryStage.setTitle(resourceBundle.getString("applicationTitle"));
 		primaryStage.setScene(mainScene);
 		primaryStage.show();
+
 	}
 
 	public static void main(String[] args) {
